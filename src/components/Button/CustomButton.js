3d8ee3df24f98456529c9a73df1dc35a4ab94854
nenadvuk <you@example.com>
2021-09-10 12:20:19 +0200

    footer

--- src/components/Button/CustomButton.js
+++ src/components/Button/CustomButton.js
@@ -3,10 +3,7 @@ import './Button.css';
 
 const CustomButton = ({ text, icon }) => {
   return (
-    <button
-      className="custom_btn"
-      /*  endicon={icon ? <div className="btn_icon_container">{icon}</div> : null} */
-    >
+    <button className="custom_btn">
       <span className="btn_text">{text}</span>
       <span className="btn_icon_container">{icon}</span>
     </button>
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from 'react';
+import './Footer.css';
+import { Typography } from '@material-ui/core';
+import myData from '../../utilities/myData';
 
 const Footer = () => {
   return (
-    <div>
-      Footer
+    <div className="footer">
+      <div className="footer_left">
+        <Typography className="footer_name">{myData.name}</Typography>
+      </div>
+      <div className="footer_right">
+        <Typography className="footer_year_of_development">2021</Typography>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
